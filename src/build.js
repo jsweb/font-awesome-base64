93d67647f3278534a71b37e46c7250080f761f07
Alex Bruno Cáceres <alxbr@alxbr-acer-w10.localdomain>
2020-11-18 12:03:47 -0300

    Build update

--- src/build.js
+++ src/build.js
@@ -22,5 +22,6 @@ const faClasses = readPath(faFree('fontawesome'))
 writeDist('fa-base', faClasses)
 
 // Build all icons
-// const allFonts = fonts.map((data) => data.code).join('\n')
+const allFonts = fonts.map((data) => data.code).join('\n')
+console.log(allFonts)
 // writeDist('fa-all', allFonts.concat('\n', faClasses))
